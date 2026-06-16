c992151977cc49ba020cb647edbe20570d70cf01
laibanooorr-cmd <laiba.nooorr@gmail.com>
2026-06-16 13:03:06 +0500

    My file

---
+++ My file.py
@@ -0,0 +1,9 @@
+BookID=7786
+print(BookID)
+Booktitle= "Think and Grow Rich"
+print(Booktitle)
+BookPrice=45.5
+print(BookPrice)
+print("Code ends here")
+Name=input("please enter your name")
+print("Welcome to course", Name)
