bfb5ac9ad2a5bf319df21062f42066491f24f08e
laibanooorr-cmd <laiba.nooorr@gmail.com>
2026-06-22 18:44:22 +0500

    practice 22 june

--- file.py
+++ file.py
@@ -1,8 +1,26 @@
-n = 5
-num = 1
-for i in range(1, n + 1):
-    row = []
-    for _ in range(i):
-        row.append(str(num))
-        num += 1
-    print(" ".join(row))
+n = 1
+if n == 1:
+    pass  # Do nothing
+for n in range(1, 3):
+    if n == 1:
+        print("n is 1")
+    elif n == 2:
+        print("n is 2")
+else:
+    print("n is neither 1 nor 2")
+    n = 3
+    if n == 3:
+        print("n is 3")
+
+    for n in range(6):
+        if n == 4:
+            break
+        print(n)
+    print("Next run")
+
+
+    for n in range(6):
+         if n == 4:
+            continue
+         print(n)
+    print("Nesxt run")
---
+++ for num.py
@@ -0,0 +1,8 @@
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+print(" The area of rectangle is:", length * width)
+input = int(input("Enter a number: "))
+print("The square of the number is:", input ** 2)
+while int > 2:
+    print("The number is greater than 2")
+    break   
