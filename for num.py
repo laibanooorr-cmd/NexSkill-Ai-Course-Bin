c3a478508c6c716063e9b2a15a52c50e41d49554
laibanooorr-cmd <laiba.nooorr@gmail.com>
2026-06-23 17:06:28 +0500

    my file 22 junr

--- for num.py
+++ for num.py
@@ -1,8 +1,5 @@
-length = float(input("Enter the length of the rectangle: "))
-width = float(input("Enter the width of the rectangle: "))
-print(" The area of rectangle is:", length * width)
-input = int(input("Enter a number: "))
-print("The square of the number is:", input ** 2)
-while int > 2:
-    print("The number is greater than 2")
-    break   
+
+Text = input("Python1 , Python2")
+print(Text.upper())
+Text = input("PYHTON , PYTHON2")
+print(Text.lower())
